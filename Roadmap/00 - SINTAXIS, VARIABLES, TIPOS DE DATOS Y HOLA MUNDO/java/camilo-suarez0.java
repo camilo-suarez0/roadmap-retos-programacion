58d0896d8c5feb444d6e3b24373252adceebb067
Camilo <kmil97@hotmail.com>
2026-08-22 19:53:32 -0500

    Change variable 'name' from 'Camilo' to 'Andres'
    Updated variable 'name' to a new value.

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/camilo-suarez0.java
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/camilo-suarez0.java
@@ -1,1 +1,18 @@
 
+//https://www.java.com/es/
+
+// Así es un comentario en una sola linea.
+
+/*
+Así es un
+comentario de
+varias lineas.
+*/
+
+/**
+Asi es un comentario
+de manera oficial en Java
+*/
+
+var name = "Camilo";
+var name = "Andres"; //Nuevo nombre en mi variable
