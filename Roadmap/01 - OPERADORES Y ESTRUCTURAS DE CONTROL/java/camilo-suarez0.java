de515badb1890773c6d0ecffdc0ce2cd8d042c71
Camilo <kmil97@hotmail.com>
2026-08-25 21:04:03 -0500

    Clean up blank lines in camilo-suarez0.java
    Removed unnecessary blank lines in the code.

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/java/camilo-suarez0.java
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/java/camilo-suarez0.java
@@ -1,10 +1,7 @@
-
-
 class camilo_suarez0{
   public static void main(String[] args){
     
     //Operadores Aritmeticos
-    
     System.out.println("Suma: 10 + 3 = " + (10 + 3));
     System.out.println("Resta: 10 - 3 = " + (10 - 3));
     System.out.println("Multiplicación: 10 * 3 = " + (10 * 3));
