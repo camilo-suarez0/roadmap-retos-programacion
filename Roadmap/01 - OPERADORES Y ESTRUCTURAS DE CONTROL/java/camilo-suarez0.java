27939d5a9383abf5b3c19f3d5e5ad715b5d5cb15
Camilo <kmil97@hotmail.com>
2026-08-25 20:50:00 -0500

    Refactor camilo-suarez0.java to include class structure

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/java/camilo-suarez0.java
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/java/camilo-suarez0.java
@@ -1,4 +1,10 @@
 
-//Operadores Aritmeticos
 
-System.out.println("Suma: 10 + 3 = " + (10 + 3));
+class camilo_suarez0{
+  public static void main(String[] args){
+    
+    //Operadores Aritmeticos
+    
+    System.out.println("Suma: 10 + 3 = " + (10 + 3));
+  }
+}
